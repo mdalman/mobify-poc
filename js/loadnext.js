574e553bfa3a7e05a2337cedb7a632be988aaf97
mdalman <mdalman@mec.ca>
2015-03-12 16:32:10 -0700

    Update loadnext.js

--- js/loadnext.js
+++ js/loadnext.js
@@ -7,7 +7,7 @@ function loadNextPage(event) {
 
         $.each(data.images, function (index, imageUrl) {
             console.log(imageUrl);
-            var optimizedUrl = ResizeImages.getImageURL(imgUrl);
+            var optimizedUrl = ResizeImages.getImageURL(imageUrl);
             console.log(optimizedUrl);
             var htmlRow = '<div class="row">' +
                 '<div class="col-md-12">' +
