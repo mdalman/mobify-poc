ea397ce8e14ea3aab7f740cc5473b40dbcee7661
mdalman <mdalman@mec.ca>
2015-03-17 17:02:52 -0700

    Update loadnext.js

--- js/loadnext.js
+++ js/loadnext.js
@@ -17,7 +17,7 @@ function loadNextPage(event) {
     event.preventDefault();
     var nextUrl = $('#load-more').attr('href');
     $.getJSON(nextUrl, function (data) {
-        $('#load-more').attr('href', data.next);
+        $('#load-more-container').attr('href', data.next);
         var items = [];
 
         $.each(data.images, function (index, imageUrl) {
