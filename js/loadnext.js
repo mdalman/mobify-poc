0a1ad9607c3924ac460469173b40123541b9280f
mdalman <mdalman@mec.ca>
2015-03-18 15:27:45 -0700

    Update loadnext.js

--- js/loadnext.js
+++ js/loadnext.js
@@ -20,7 +20,7 @@ function insertFallbackImageUrl(e){
               
     console.log('error world');
     var $image = $(e.target);
-    $image.off('error'); //If we put another url in src that 404's we don't want an infinite loop
+//    $image.off('error'); //If we put another url in src that 404's we don't want an infinite loop
     
     console.log($image);
     var fallbackSrc = $image.attr('data-fallback-src')
