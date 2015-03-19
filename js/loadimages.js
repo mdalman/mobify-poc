264738c688716ed5ce6cdaa56ff719e8143b9b40
mdalman <mdalman@mec.ca>
2015-03-19 02:50:10 -0700

    Update loadimages.js

--- js/loadimages.js
+++ js/loadimages.js
@@ -1,79 +1,84 @@
-qualities = { 3:{'zoom':48,'regular':35},//Morgan made up the 'default' from a nexus 5, 'zoom' value is a guess
-              2:{'zoom':60,'regular':48},//Both values are guesses
-              1:{'zoom':70,'regular':60}//Both values are guesses
+qualities = {
+    3: {
+        'zoom': 48,
+        'regular': 35
+    }, //Morgan made up the 'default' from a nexus 5, 'zoom' value is a guess
+    2: {
+        'zoom': 60,
+        'regular': 48
+    }, //Both values are guesses
+    1: {
+        'zoom': 70,
+        'regular': 60
+    } //Both values are guesses
 };
 
-function getUrlOverride(queryParameter,original){
+function getUrlOverride(queryParameter, original) {
     var override = purl(window.location).param(queryParameter);
-    if (typeof override === "undefined"){
-                  return original;
-    }
-    else{
-                  return override;
+    if (typeof override === "undefined") {
+        return original;
+    } else {
+        return override;
     }
 }
 
-function insertFallbackImageUrl(e){
-              
+function insertFallbackImageUrl(e) {
+
     console.log('error world');
     var $image = $(e.target);
     $image.off('error'); //If we put another url in src that 404's we don't want an infinite loop
-    
+
     console.log($image);
     var fallbackSrc = $image.attr('data-fallback-src');
     console.log(fallbackSrc);
-    $image.attr('src',fallbackSrc);
+    $image.attr('src', fallbackSrc);
 }
 
-function roundToStep(rawValue,step){
+function roundToStep(rawValue, step) {
     return Math.ceil((rawValue) / step) * step;
 }
 
 
-function getImageUrl(originalUrl,width,quality){
+function getImageUrl(originalUrl, width, quality) {
     var format;
     var pixelStep = 100;
-    
-    var steppedWidth = roundToStep(width,pixelStep);
-    
-    if(Modernizr.webp){
-                  format = 'webp';
+
+    var steppedWidth = roundToStep(width, pixelStep);
+
+    if (Modernizr.webp) {
+        format = 'webp';
+    } else {
+        format = 'jpg';
     }
-    else{
-                 format = 'jpg';
-    }
-    return '//ir0.mobify.com/project-mobify-poc/c8/'+format+quality+'/'+steppedWidth+'/'+originalUrl;
+    return '//ir0.mobify.com/project-mobify-poc/c8/' + format + quality + '/' + steppedWidth + '/' + originalUrl;
 }
 
 
-function getPixelRatio(){
-              var pixelRatio;
+function getPixelRatio() {
+    var pixelRatio;
 
-                 if (typeof devicePixelRatio === "undefined")
-                 {
-                     pixelRatio = 1;     
-                 }
-                 else{
-                     pixelRatio = devicePixelRatio;
-                 }
-                 return getUrlOverride('pr',pixelRatio,)
+    if (typeof devicePixelRatio === "undefined") {
+        pixelRatio = 1;
+    } else {
+        pixelRatio = devicePixelRatio;
+    }
+    return getUrlOverride('pr', pixelRatio );
 }
 
-function getQuality(pixelRatio,zoom){
+function getQuality(pixelRatio, zoom) {
     var quality;
     var roundedPixelRatio = Math.floor(pixelRatio); //Error on the side of quality
     var qualityLookupKey = Math.min(3, roundedPixelRatio); //pixel ratio higher than 3X will get same quality as 3X
     qualityLookupKey = Math.max(1, roundedPixelRatio); //pixel ratio less than 1X will get same quality as 1X
 
     var qualityObject = qualities[qualityLookupKey];
-    
-    if (zoom){
+
+    if (zoom) {
         quality = qualityObject.zoom;
-    }
-    else{
+    } else {
         quality = qualityObject.regular;
     }
-    return getUrlOverride('q',quality);
+    return getUrlOverride('q', quality);
 }
 
 
@@ -88,16 +93,16 @@ function loadImages(event) {
 
         var pixelRatio = getPixelRatio();
         var quality = getQuality(pixelRatio);
-       
+
         var maxWidth = Math.ceil(cssWidth * pixelRatio);
 
-        var optimizedUrl = getImageUrl(dataSrc,maxWidth,quality);
+        var optimizedUrl = getImageUrl(dataSrc, maxWidth, quality);
         console.log(optimizedUrl);
-        if(window.location.href.indexOf("fallback") > -1) {
-              optimizedUrl = 'fail://this-is-not-a-valid-url-because-you-specified-fallback.jpg';
-              }
-        
+        if (window.location.href.indexOf("fallback") > -1) {
+            optimizedUrl = 'fail://this-is-not-a-valid-url-because-you-specified-fallback.jpg';
+        }
+
         $image.removeAttr('data-src').attr('src', optimizedUrl);
-        $image.on('error',insertFallbackImageUrl);
+        $image.on('error', insertFallbackImageUrl);
     });
 }
