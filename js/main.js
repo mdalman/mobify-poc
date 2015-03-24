d405edea9011f1bcf52c9a94190edcff75a5264e
mdalman <mdalman@mec.ca>
2015-03-24 07:09:07 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,47 +1,52 @@
-function getTotalImageBandwidth(elements){
+function getTotalImageBandwidth(elements) {
     var sum = 0;
-    var requests = []
+    var requests = [];
     $('img').each(function (index, img) {
-    	try{
-	        var urlToCheck = img.src;
-	        if(urlToCheck !== ''){
-                var request = $.getJSON("http://69.164.195.251/filesize/", 
-                          {'urltocheck':urlToCheck},
-                          function(data){sum+=data.size;});
-                requests.push(request);                
-	        }
-    	}
-    	catch(error){console.log(error);}
-    });
-    
-    $.when.apply(this,requests).then(function( x ) {
-      var kbs = Math.ceil(sum/Math.pow(2, 10));
-      
-      elements.html(kbs+'kb');
-    });
- }
+        try {
+            var urlToCheck = img.src;
+            if (urlToCheck !== '') {
+                var request = $.getJSON("http://69.164.195.251/filesize/", {
+                    'urltocheck': urlToCheck
+                },
 
-function getFallBackImageUrl(imageUrl){
-    return imageUrl.replace('/high-res/','/low-res/');
+                function (data) {
+                    sum += data.size;
+                });
+                requests.push(request);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    $.when.apply(this, requests).then(function (x) {
+        var kbs = Math.ceil(sum / Math.pow(2, 10));
+
+        elements.html(kbs + 'kb');
+    });
+}
+
+function getFallBackImageUrl(imageUrl) {
+    return imageUrl.replace('/high-res/', '/low-res/');
 }
 
 function loadNextPage(event) {
     event.preventDefault();
     var nextUrl = $('#load-more').attr('href');
     $.getJSON(nextUrl, function (data) {
-        $('#load-more').attr('href', data.next.replace(/^https?:\/\//,'//'));
+        $('#load-more').attr('href', data.next.replace(/^https?:\/\//, '//'));
         var items = [];
 
         $.each(data.images, function (index, imageUrl) {
             var fallbackImageUrl = getFallBackImageUrl(imageUrl);
-            var quickSrc = getImageUrl(imageUrl, 100, 50,'jpg');
+            var quickSrc = getImageUrl(imageUrl, 100, 50, 'jpg');
             var htmlRow = '<div style="height:auto;" class="col-xs-6 col-sm-4 col-md-3 col-lg-2">' +
-                            '<img class="img-responsive" ' +
-                                 'data-src="' + imageUrl + '" ' +
-                                 'src="' + quickSrc + '" ' +
-                                 'data-fallback-src="' + fallbackImageUrl + '" ' +
-                                 '/>' +
-                          '</div>';
+                '<img class="img-responsive" ' +
+                'data-src="' + imageUrl + '" ' +
+                'src="' + quickSrc + '" ' +
+                'data-fallback-src="' + fallbackImageUrl + '" ' +
+                '/>' +
+                '</div>';
 
             items.push(htmlRow);
         });
@@ -54,45 +59,45 @@ function loadNextPage(event) {
 
 }
 
-function clickImageHandler(event){
-	var $target = $(event.target);
-	var imageUrl = $target.attr('data-src');
-	var imageSrc = $target.attr('src');
-	
-	var fallbackImageUrl = $target.attr('data-fallback-src');
+function clickImageHandler(event) {
+    var $target = $(event.target);
+    var imageUrl = $target.attr('data-src');
+    var imageSrc = $target.attr('src');
 
-	$('.img-preview').off('error').removeAttr('data-tiny-src').attr('src',imageSrc).attr('data-src', imageUrl).attr('data-fallback-src',fallbackImageUrl);    	
-	$('#img-modal').modal('show'); 
-    	
-    	$('#img-modal').on('shown.bs.modal', function (e) {
-  		loadImages();
-	})
-        
-}
+    var fallbackImageUrl = $target.attr('data-fallback-src');
 
-function registerImageClicks(){
-	$('.img-responsive').on('click', clickImageHandler);
-}
+    $('.img-preview').off('error').removeAttr('data-tiny-src').attr('src', imageSrc).attr('data-src', imageUrl).attr('data-fallback-src', fallbackImageUrl);
+    $('#img-modal').modal('show');
 
-$(document).ready(function() {
-    $('select[name="q"] option[value="'+$.url(window.location).param('q')+'"]').prop('selected',true);
-    $('select[name="pr"] option[value="'+$.url(window.location).param('pr')+'"]').prop('selected',true); 
-    $('select[name="fb"] option[value="'+$.url(window.location).param('fb')+'"]').prop('selected',true); 
-    $('select[name="v"] option[value="'+$.url(window.location).param('v')+'"]').prop('selected',true);     
-	
-    $('#pixel-ratio').append(getPixelRatio()+'X');
-    $('#zoom-quality').append(getQuality(getPixelRatio(),true)+'%');
-    $('#regular-quality').append(getQuality(getPixelRatio(),false)+'%');    
-      
- 
-    Modernizr.on('webp', function (result) {//wait until we know if webp is supported
+    $('#img-modal').on('shown.bs.modal', function (e) {
         loadImages();
-	});
-  
-  
-    
+    });
+
+}
+
+function registerImageClicks() {
+    $('.img-responsive').on('click', clickImageHandler);
+}
+
+$(document).ready(function () {
+    $('select[name="q"] option[value="' + $.url(window.location).param('q') + '"]').prop('selected', true);
+    $('select[name="pr"] option[value="' + $.url(window.location).param('pr') + '"]').prop('selected', true);
+    $('select[name="fb"] option[value="' + $.url(window.location).param('fb') + '"]').prop('selected', true);
+    $('select[name="v"] option[value="' + $.url(window.location).param('v') + '"]').prop('selected', true);
+
+    $('#pixel-ratio').append(getPixelRatio() + 'X');
+    $('#zoom-quality').append(getQuality(getPixelRatio(), true) + '%');
+    $('#regular-quality').append(getQuality(getPixelRatio(), false) + '%');
+
+
+    Modernizr.on('webp', function (result) { //wait until we know if webp is supported
+        loadImages();
+    });
+
+
+
     $('#load-more').click(loadNextPage);
-    
-    
+
+
     registerImageClicks();
 });
